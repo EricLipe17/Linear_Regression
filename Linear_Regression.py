ba60d09b6b0af1793efc45263df9f14151603f2f
Eric <EricLipe17@gmail.com>
2019-03-26 06:34:03 -0600

    Quick update.

---
+++ Linear_Regression.py
@@ -0,0 +1,19 @@
+# import pandas as pd
+# import numpy as np
+# import matplotlib.pyplot as plt
+#
+# class LinearRegression():
+#
+#     def __init__(self):
+#         # self.line = 0
+#
+#     def fit(self, data, targets):
+#         # Use your derivatives here to find the optimal solution
+#         # 'data' here is the data, and 'targets' will be the y = 5x + 3 you create in your main
+#         # Return the line of best fit
+#
+#     def accuracy(self, data, targets):
+#         # Use this method to calculate the R^2
+#
+#     def plot(self, data):
+#         # Use this method to produce a plot of the line of best fit vs a scatter plot of the data
